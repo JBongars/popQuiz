979d75eef5d34be6ae0ada3d551af1be8415de51
minh <minh@voyagerinnovation.com>
2017-08-31 12:05:12 +0800

    reset project (to do at home)

--- client/app/MainCtrl.js
+++ client/app/MainCtrl.js
@@ -14,75 +14,10 @@
         function PopQuizCtrl($http) {
             var self = this; // vm
     
-            self.quiz = {        };
-            self.quizCount = 0;
-            self.totalNum = 0;
-            
-            //finalanswer to be compiled on the screen
-            self.finalanswer = {
-                id: 0,
-                value: "",
-                comments: ""
-            };
-
-            self.data = {}; //data container
-    
-            //Initiation of the page
-            self.initForm = function() {
-                $http.get("/popquizes")
-                    .then(function(result) {
-                        //console.log(result);
-                        self.data = result.data;
-                        console.log(JSON.stringify(self.data));
-                    }).catch(function(e) {
-                        console.log(e);
-                    });
-            };
-
-            self.quiz = self.data.quiz;
-            self.totalNum = self.totalNum;
-            
-            //When User clicks the next button
-            self.nextForm = function(){
-                //empty
+            self.initForm = function(){
+                
             }
 
-            //When User clicks the submit button
-            self.submitQuiz = function() {
-
-                console.log("submitQuiz !!!");
-    
-                //Update id number for quiz being posted
-                self.finalanswer.id = self.quiz.id;
-            
-                $http.post("/submit-quiz", self.finalanswer)
-                    .then(function(result) {
-                        //console.log(result);
-    
-                        self.finalanswer.isCorrect = result.data.isCorrect
-    
-                        //find out more
-                        self.summaryAnswers.push(Object.assign({}, 
-                            self.finalanswer, 
-                            self.quiz
-                        ));
-    
-                    }).catch(function(e) {
-                        console.log(e);
-                    });
-            };
-    
-            self.resetQuizes = function(){
-    
-                console.log("Success!!");
-    
-                $http.get("/reset-quiz")
-                    .then(function(result){
-                        console.log(JSON.stringify(result.data));
-                    }).catch(function(e){
-                        console.log(e);
-                    });
-            };
         }
     
     })();
